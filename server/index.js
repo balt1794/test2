a85429c852c678a1fde0ab252904d61dd274a8de
balt1794 <68751264+balt1794@users.noreply.github.com>
2022-09-24 20:13:56 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ origin: '*' }));
 
 
-const YOUR_DOMAIN = 'http://localhost:3000';
+const YOUR_DOMAIN = 'https://main.dqqa3112ms9x4.amplifyapp.com/';
 
 app.get("/", async (req, res) => {
   res.send('Node Server Working')
